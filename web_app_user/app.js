d59eb6dc44fa04c3a89fe4657543745e383773ae
Ariq Fakhrizaki <ariqfakhrizakip@gmail.com>
2026-04-07 14:30:47 +0700

    add firebase config

---
+++ web_app_user/app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            projectId: ""
+        };
+
+        firebase.initializeApp(firebaseConfig);
+        const db = firebase.database();
